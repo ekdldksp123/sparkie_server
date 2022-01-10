241f589644e801a87cb5a105a73bbe940ecee5ae
mynamesunpower <mynamesunpower@gmail.com>
2022-01-11 00:11:18 +0900

    feat: 프로필 작성

--- src/lib/api/user/registerUser.ts
+++ src/lib/api/user/registerUser.ts
@@ -1,3 +1,17 @@
-export default () => {
-  console.log('registerUser');
+import { Request, Response } from 'express';
+import { client } from '../../mongoClient';
+import { Db } from 'mongodb';
+
+export default async (req: Request, res: Response) => {
+  const userProfile = req.body;
+  try {
+    const db: Db = await client.db();
+    const result = await db.collection('user').insertOne(userProfile);
+
+    res.status(200);
+    res.send(result);
+  } catch (e) {
+    res.status(500);
+    res.send(`Server Error! sry. \n` + e);
+  }
 };
