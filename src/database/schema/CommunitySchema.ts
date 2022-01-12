830d234e4c2c33f56dfd8446c4cb26498a3fa68a
ekdldksp123 <ekdldksp123@naver.com>
2022-01-12 23:00:30 +0900

    [Update] create database connection when server is loaded

---
+++ src/database/schema/CommunitySchema.ts
@@ -0,0 +1,23 @@
+import mongoose, {Schema, Document, Model} from "mongoose";
+
+// Define Schemes
+const CommentSchema = new Schema({
+    seq: Schema.Types.ObjectId,
+    writer: {type: String, required: true},
+    date: {type: String, required: true},
+    profile: String,
+    content: {type: String, required: true},
+});
+
+const PostSchema = new Schema({
+    key: Schema.Types.ObjectId,
+    title:{type: String, required: true},
+    date: {type: String, required: true},
+    writer: {type: String, required: true},
+    content: {type: String, required: true},
+    likes: Number,
+    comments: [CommentSchema], 
+});
+
+// Create Model & Export
+module.exports = mongoose.model('Post', PostSchema);
