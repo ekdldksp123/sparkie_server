f5f48a8130a59b1defdce03d2c227bf0705a69a1
ekdldksp123 <ekdldksp123@naver.com>
2022-01-13 00:01:48 +0900

    [Add] add service module to community process

--- services/communityService.ts
+++ services/communityService.ts
@@ -1,1 +1,16 @@
 /* 커뮤니티 DB 관련 작업 서비스 */
+import mongoose, {Schema, Document, Model} from "mongoose";
+
+const Post = require('../src/database/schema/CommunitySchema');
+
+export const getPosts = (key:string) => {
+    
+}
+
+export const writePost = () => {
+
+}
+
+export const addComment = () => {
+    
+}
